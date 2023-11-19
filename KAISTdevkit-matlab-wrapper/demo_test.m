a46f52b35a5c3426e97adf67da759bd63ebc37ee
XueZ-phd <zxue2019@zju.edu.cn>
2023-11-19 19:42:48 +0800

    update readme

--- KAISTdevkit-matlab-wrapper/demo_test.m
+++ KAISTdevkit-matlab-wrapper/demo_test.m
@@ -6,7 +6,6 @@ for epoch=3
     dtDir = sprintf('%s%d','../mmdetection/runs/FasterRCNN_vgg16_channelRelation_dscSEFusion_similarityMax_1/epoch_/epoch_', epoch);
     %% specify path of groundtruth annotaions
     gtDir = './annotations_KAIST_test_set';
-%     gtDir = 'E:\pyDemo\cross-modality-det\mmdetection\runs_llvip\FasterRCNN_r50wMask_ablation_onlyFFM_ROIFocalLoss5_CIOU20_cosineSE_dcnGWConvGlobalCC_640x512';
     %% evaluate detection results
     kaist_eval_full(dtDir, gtDir, false, false);
 end
